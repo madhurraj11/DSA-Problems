0d64856970fede4b448ec7e4ca0b9813ca87f8c8
Madhur Chauhan <madhurchauhan001256@gmail.com>
2024-08-13 21:54:00 +0530

    Initial commit

---
+++ Pattern1.js
@@ -0,0 +1,45 @@
+// row="";
+// for(let i =0; i<=5; i++){
+//     for(let j=0; j<=i; j++){
+//         row += "*";
+//     }
+//     row += "\n";
+// }
+// console.log(row);
+
+
+// row="";
+// for(let i =0; i<=5; i++){
+//     for(let j=0; j<=5-i; j++){
+//         row += "*";
+//     }
+//     row += "\n";
+// }
+// console.log(row);
+
+// row="";
+// for(let i =0; i<=5; i++){
+//     for(let j=0; j<= 5-i; j++){
+//         row += " ";
+//     }
+
+//     for(let k=0; k < i; k++){
+//         row += "*";
+//     }
+//     row += "\n";
+// }
+// console.log(row);
+
+// row="";
+// for(let i =0; i<=5; i++){
+//     for(let j=0; j< i; j++){
+//         row += " ";
+//     }
+
+//     for(let k=0; k < 5-i; k++){
+//         row += "*";
+//     }
+//     row += "\n";
+// }
+// console.log(row);
+
