6fe5acc8cad2b7eace1d04c3f31eb59d59c4c54b
Madhur Chauhan <madhurchauhan001256@gmail.com>
2024-08-14 21:34:35 +0530

    first commit

---
+++ Pattern2.js
@@ -0,0 +1,31 @@
+// row="";
+// for(let i = 0; i < 5; i++){
+//     for(let j = 0; j < 5-i; j++){
+//         row += " ";
+//     }
+//     for(let k=0; k <= i; k++){
+//         row += " *";
+//     }
+//     row += "\n";
+// }
+// console.log(row);
+
+// row="";
+// for(let i = 1; i <= 5; i++){
+//     for(let j = 1; j <= i; j++){
+//         row += j;
+//     }
+//     row += "\n";
+// }
+// console.log(row);
+
+// row="";
+// let num=1;
+// for(let i = 1; i <= 5; i++){
+//     for(let j = 1; j <= i; j++){
+//         row += num+" ";
+//         num++;
+//     }
+//     row += "\n";
+// }
+// console.log(row);
