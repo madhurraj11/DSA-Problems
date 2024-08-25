67575b9d2e03468af0c02b51a98cabe1efda1a70
Madhur Chauhan <madhurchauhan001256@gmail.com>
2024-08-25 11:41:12 +0530

    first commit

---
+++ FindLongestString.js
@@ -0,0 +1,13 @@
+let str = "abcdefabcdsvhggabc";
+let max_length = 0;
+let start = 0;
+let newStr = "";
+ for(let i = 0; i < str.length; i++){
+    if(!newStr.includes(str.charAt(i))){
+        newStr += str.charAt(i);
+        if(newStr.length > max_length){
+            max_length = newStr.length;
+        }
+    }
+ }
+console.log(newStr, max_length);
